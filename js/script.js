eb3961de74b607795f62c443f714d5b18465e5af
valtebb <45037496+valtebb@users.noreply.github.com>
2021-07-15 02:17:37 +0100

    Updated JS file

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@
 const navList = document.querySelector('ul');
 // const sections = document.querySelectorAll('section');
 const sections = Array.from(document.getElementsByTagName("section"));
-console.log(sections);
-const nav = document.querySelectorAll('.container ul li');
-console.log(nav);
+
 
 for(section of sections) {
   let listItem = document.createElement('li');
+  listItem.setAttribute('class', `#${section.getAttribute('id')}`);
   let link = document.createElement('a');
   link.textContent = section.dataset.nav;
+  link.setAttribute('id', `#${section.getAttribute('id')}`);
   listItem.appendChild(link);
   link.addEventListener('click', () => {
     section.scrollIntoView({behavior: 'smooth'})
@@ -23,14 +23,43 @@ const listerner = function(){
   sections.forEach(zone => {
     zone.getBoundingClientRect();
     if(zone.getBoundingClientRect().top >= 83 && zone.getBoundingClientRect().top <= 376){
-      zone.classList.add('active');
+      zone.classList.add('active__section');
     }
     else{
-      zone.classList.remove('active');
+      zone.classList.remove('active__section');
     }
   })
 }
 document.addEventListener('scroll', listerner);
+
+
+const nav = document.querySelectorAll('nav .container ul li');
+console.log(nav);
+
+window.addEventListener('scroll', ()=> {
+  let current = ''; 
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    // const sectionHeight = section.clientHeight;
+    if(pageYOffset >= sectionTop){
+      current = section.getAttribute('id');
+    }
+  })
+  console.log(current);
+  nav.forEach(li => {
+    li.classList.remove('active');
+    if(li.classList.contains(current)){
+      li.classList.add('active');
+    }
+  })
+
+})
+
+
+
+
+
+
 
 
 
